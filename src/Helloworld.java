5731cf124e7fc338ff7889bfcc4e601a669bde22
배경훈 <@DESKTOP-45VGVCM>
2020-03-19 21:52:12 +0900

    Creat a menu

---
+++ src/Helloworld.java
@@ -0,0 +1,14 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("*** Grade Management System Menu ***");
+		System.out.println("1. Add Grade");
+		System.out.println("2. Delete Grade");
+		System.out.println("3. Edit Grade");
+		System.out.println("4. View Grade");
+		System.out.println("5. Exit"); 	
+		System.out.println("Select one number between 1 - 5");
+	}
+
+}
