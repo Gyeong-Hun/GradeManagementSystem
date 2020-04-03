8193c2599b472661c5aa28173d7d2faed460746c
배경훈 <@DESKTOP-45VGVCM>
2020-04-04 01:47:01 +0900

    modified the edit menu

--- src/GradeManager.java
+++ src/GradeManager.java
@@ -37,7 +37,36 @@ public class GradeManager {
 		System.out.print("Subject ID:");
 		int subjectid = input.nextInt();
 		if (grade.subjectid == subjectid) {
-			System.out.println("the subject to be edited is" + subjectid);
+			int num = -1;
+			while (num != 5) {
+				System.out.println("- Grade Info Edit Menu -");
+				System.out.println("1. Edit Subject Name"); //그 학생의 성적(성적에는 과목, 학점, 받은성적이 들어갈 예정이다.)
+				System.out.println("2. Edit Subject Id");
+				System.out.println("3. Edit Subject credit");
+				System.out.println("4. Edit Your Grade");
+				System.out.println("5. Exit");
+				System.out.println("Select one number between 1 - 5 :");
+				num = input.nextInt();
+				if (num == 1) {
+					System.out.print("Input Subject Name:");
+					grade.subjectname = input.next();
+				}
+				else if (num == 2) {
+					System.out.print("Input Subject Id:");
+					grade.subjectid = input.nextInt();
+				}
+				else if (num == 3) {
+					System.out.print("Input Subject credit:");
+					grade.subjectcredit = input.nextInt();
+				}
+				else if (num == 4) {
+					System.out.print("Input your grade:");
+					grade.yourgrade = input.nextDouble();
+				}
+				else {
+					continue;
+				}
+			}
 		}
 	}
 	
